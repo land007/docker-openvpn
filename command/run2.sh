74f361f3fa5ab6e31ccb215ca6328dd20e57bd3c
阿七 <yiqiujia@hotmail.com>
2020-12-06 10:12:20 +0800

    test

--- command/run2.sh
+++ command/run2.sh
@@ -1,6 +1,6 @@
 docker stop openvpn
 docker rm openvpn
 OVPN_DATA="ovpn-data"
-docker run --restart always -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN --restart always --name openvpn kylemanna/openvpn
+docker run --restart always -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN --restart always --name openvpn land007/openvpn
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full CLIENTNAME nopass
 docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient CLIENTNAME > CLIENTNAME.ovpn
